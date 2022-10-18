c451ad18f1861f23f9693dca6cee07e3eb702414
Rayan-bhaa <RayanBhaa@icloud.com>
2022-10-18 10:06:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,8 @@
 
 /**
  * main - prints Putchar
- *
- * Description: prints _putchar
- *
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -19,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
