60c72b690b5fa2cb248e0ac63ab269fc950bf900
Rayan-bhaa <RayanBhaa@icloud.com>
2022-10-24 12:28:04 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void)
 {
 
 int i, j;
-for (i = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
@@ -18,6 +18,6 @@ for (i = 1; i <= 10; i++)
 			_putchar('1');
 			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
